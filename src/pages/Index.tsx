0bd3f2b02549cd12cc241c194f677a26b6b3c105
Eric Turner <ericturner@Erics-iMac.attlocal.net>
2023-03-13 21:25:03 -0400

    types to index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,8 +2,17 @@ import { useLoaderData, Form } from "react-router-dom";
 import Post from "../components/Post";
 import "../App.css"
 
-const Index = (props) => {
-  const todos = useLoaderData();
+interface Todo {
+  id: number;
+  make: string;
+  model: string;
+  year: number;
+  color: string;
+  price: number;
+}
+
+const Index: React.FC = (props) => {
+  const todos: Todo[] = useLoaderData() as Todo[];
 
   return <>
    <h2 className='title'>Post Your Car</h2>
@@ -15,7 +24,7 @@ const Index = (props) => {
           <input type="text" name="model" placeholder="model"/>
           <input type="text" name="year" placeholder="year"/>
           <input type="text" name="color" placeholder="color"/>
-          <input type="text" name="price" placeholder="price"/>
+          <input type="number" name="price" placeholder="price"/>
           <button>Create New Car</button>
       </Form>
   </div>
