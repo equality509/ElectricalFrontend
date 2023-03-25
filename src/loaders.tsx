543a06cd6d892692a918c74ea72ddd514df18b66
Eric Turner <ericturner@Erics-iMac.attlocal.net>
2023-03-25 17:52:46 -0400

    all but router and actions typed

--- src/loaders.tsx
+++ src/loaders.tsx
@@ -1,17 +1,43 @@
+// import url from "./urls";
+
+// export async function IndexLoader(){
+//     const response = await fetch(url)
+//     const data = await response.json()
+
+//     console.log("INDEX LOADER:", data)
+
+//     return data
+// }
+
+// export async function ShowLoader({params}){
+//     const response = await fetch(url + "/" + params.id + "/")
+//     const data = await response.json()
+//     console.log("SHOW LOADER:", data)
+//     return data
+// }
+
 import url from "./urls";
 
-export async function IndexLoader(){
-    const response = await fetch(url)
-    const data = await response.json()
-
-    console.log("INDEX LOADER:", data)
-
-    return data
+interface Post {
+  projectName: string;
+  location: string;
+  numberOfWorkers: number;
+  manager: string;
+  _id: string;
 }
 
-export async function ShowLoader({params}){
-    const response = await fetch(url + "/" + params.id + "/")
-    const data = await response.json()
-    console.log("SHOW LOADER:", data)
-    return data
+export async function IndexLoader(): Promise<Post[]> {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log("INDEX LOADER:", data);
+
+  return data;
+}
+
+export async function ShowLoader({ params }: { params: { id: string } }): Promise<Post> {
+  const response = await fetch(`${url}/${params.id}/`);
+  const data = await response.json();
+  console.log("SHOW LOADER:", data);
+  return data;
 }
--- src/pages/Show.tsx
+++ src/pages/Show.tsx
@@ -1,18 +1,32 @@
 import { Link, useLoaderData, Form } from "react-router-dom";
 import "../App.css"
 
-const Show = (props) => {
-  const post = useLoaderData();
+interface Post {
+  _id: string;
+  projectName: string;
+  location: string;
+  numberOfWorkers: number;
+  manager: string;
+}
+
+const Show = () => {
+  const post = useLoaderData() as Post;
 
   //styles
-  const div = {
-    textAlign: "center",
-    border: "3px solid",
-    width: "40",
-    margin: "30px auto",
-  };
+  // const div = {
+  //   textAlign: "center",
+  //   border: "3px solid",
+  //   width: "40",
+  //   margin: "30px auto",
+  // };
 
  
+  const div: React.CSSProperties = {
+        textAlign: "center",
+        border: "3px solid",
+        width: "40",
+        margin: "30px auto",
+      };
 
   return (
     <div style={div} >
@@ -60,3 +74,75 @@ const Show = (props) => {
 };
 
 export default Show;
+
+
+// import { Link, useLoaderData } from "react-router-dom";
+// import "../App.css"
+
+// interface Post {
+//   _id: string;
+//   projectName: string;
+//   location: string;
+//   numberOfWorkers: number;
+//   manager: string;
+// }
+
+// const Show = () => {
+//   const post = useLoaderData() as Post;
+
+//   //styles
+//   const div: React.CSSProperties = {
+//     textAlign: "center",
+//     border: "3px solid",
+//     width: "40",
+//     margin: "30px auto",
+//   };
+
+ 
+
+//   return (
+//     <div style={div} >
+//       <h1 className= "show-letters">{post.projectName}</h1>
+//       <h2 className= "show-letters">{post.manager}</h2>
+      
+//       <div>
+//         <h2 className= "show-letters">Update this Project</h2>
+//         <form action={`/update/${post._id}`} method="post">
+//           <input className= "form-letters"
+//             type="text"
+//             name="projectName"
+//             placeholder="projectName"
+//             defaultValue={post.projectName}
+//           />
+//           <input className= "form-letters"
+//             type="text"
+//             name="location"
+//             placeholder={post.location}
+//             defaultValue={post.location}
+//           />
+//           <input className= "form-letters"
+//             type="number"
+//             name="numberOfWorkers"
+//             placeholder="numberOfWorkers"
+//             defaultValue={post.numberOfWorkers}
+//           />
+//           <input className= "form-letters"
+//             type="text"
+//             name="manager"
+//             placeholder="manager"
+//             defaultValue={post.manager}
+//           />
+//           <button>Update Car</button>
+//         </form>
+//         <form action={`/delete/${post._id}`} method="post">
+//             <button>Delete Car</button>
+//         </form>
+//       </div>
+//       <Link to="/">
+//         <button>Go Back</button>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default Show;
